70aff7066fe15f6fffe08bd5fccccc2ea7119fa8
tamitavares <tamiris.lemes18@gmail.com>
2022-04-11 17:40:00 -0300

    alteração

---
+++ script.js
@@ -0,0 +1,10 @@
+const animais = document.getElementById('animais');
+console.log(animais);
+
+const gridSection = document.getElementsByClassName('grid-section');
+console.log(gridSection);
+
+const primeiraLi = document.querySelector('li');
+console.log(primeiraLi)
+
+cont linksInternos = document.querySelector('[href^="#"]');
